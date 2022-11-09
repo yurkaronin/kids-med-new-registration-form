43beb886cb1a3a29b227491942ae5e0ad5ae2aaa
RoninsWork <yurkaronin@gmail.com>
2022-11-09 18:40:55 +0300

    очередной ремейк форм

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Глобальные переменные
   const onlineForm = document.querySelector('.online-form');
-  const onlineFormCall = document.querySelector('.online-form-call');
-  // const onlineFormContent = document.querySelector('.online-form__content');
+  const onlineForms = document.querySelectorAll('.online-form');
   const onlineFormCloseBTN = document.querySelector('.online-form__close-btn');
   const calendarElem = document.querySelector('#calendar');
-
+  const callBtns = document.querySelectorAll('.call-btn');
+  const pickDateBtn = document.querySelector('#pick-date-btn');
+  let calendarWrap = document.querySelector('#calendar-wrap');
   // инициализация кастомных select в форме регистрации
   const select1 = new ItcCustomSelect('#select-1');
   const select2 = new ItcCustomSelect('#select-2');
@@ -31,60 +32,54 @@ document.addEventListener('DOMContentLoaded', () => {
     };
   };
 
-  // слушаем клики
-  function liseneClick() {
-    document.addEventListener('click', (e) => {
-      let clickTarget = e.target;
+  // слушаем событие клика по кнопкам
+  for (let item of callBtns) {
+    item.addEventListener('click', function () {
 
-      if (clickTarget === onlineFormCloseBTN) {
-        dialogItem.classList.remove('open');
-        document.body.classList.remove('scroll-off');
-      } else if (clickTarget.classList.contains('online-form')) {
-        if (clickTarget.classList.contains('open')) {
-          dialogItem.classList.remove('open');
+      let box = item.getAttribute('data-btn-num');
+      let dialogWindow = document.querySelector(`#online-form-${box}`);
+
+      dialogWindow.classList.add('open');
+      document.body.classList.add('scroll-off');
+
+      if (box === '1') {
+        // пока что буду вызывать календарь тут, для тестов
+        // createCalendar(calendar, 2022, 11);
+      };
+
+      document.addEventListener('click', (e) => {
+        let clickTarget = e.target;
+
+        if (clickTarget === onlineFormCloseBTN) {
+          dialogWindow.classList.remove('open');
           document.body.classList.remove('scroll-off');
+        } else if (clickTarget.classList.contains('online-form')) {
+          if (clickTarget.classList.contains('open')) {
+            dialogWindow.classList.remove('open');
+            document.body.classList.remove('scroll-off');
+          };
         };
-      };
+
+      });
+
     });
-
-  }
-
-  // события показа формы регистрации на странице
-  document.addEventListener('click', (e) => {
-
-    // показ формы
-    let clickTarget = e.target;
-
-    if (clickTarget === onlineFormCall) {
-      onlineForm.classList.add('open');
-      document.body.classList.add('scroll-off');
-
-      // пока что буду вызывать календарь тут, для тестов
-      createCalendar(calendar, 2022, 11);
-
-    } else if (clickTarget === onlineFormCloseBTN) {
-      onlineForm.classList.remove('open');
-      document.body.classList.remove('scroll-off');
-    } else if (clickTarget.classList.contains('online-form')) {
-      if (clickTarget.classList.contains('open')) {
-        onlineForm.classList.remove('open');
-        document.body.classList.remove('scroll-off');
-      };
-    };
-
-    if (clickTarget.id === 'js-btn-proof') {
-      let dialogItem = document.querySelector('#js-modal-proof');
-      dialogItem.classList.add('open');
-      document.body.classList.add('scroll-off');
-      liseneClick();
-
-    }
+  };
 
 
+  // работаем с кнопкой вызова календаря
+  pickDateBtn.addEventListener('click', () => {
+    // console.log('111');
+    calendarWrap.classList.remove('hide');
+    createCalendar(calendar, 2022, 11);
   });
+
+
+
+  // data-set-btn
 
   // подключаем скрипты тут
   include("./js/parts/calendar.js");
+  // include("./js/parts/callBtns.js");
 
   // ВРЕМЕННО! Удалить на продакшене!  ////////////
   document.addEventListener('click', e => console.log(e.target));
